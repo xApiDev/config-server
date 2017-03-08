0ff40d5bb174ada7e1c90ab0f749d924bd5a1597
Sergio Alonso <sergio.alonsocastano@vassit.co.uk>
2017-03-08 12:35:00 +0000

    Initial commit

---
+++ src/test/java/com/xapi/ConfigServerApplicationTests.java
@@ -0,0 +1,23 @@
+package com.xapi;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.config.server.environment.EnvironmentController;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConfigServerApplicationTests {
+	@Autowired
+	private EnvironmentController controller;
+
+	@Test
+	public void contextLoads() {
+		assertThat(controller).isNotNull();
+	}
+
+}
